6f47eea5f41e5813119f9aaf4eeb4253da71c414
Jan <jandevs27@outlook.com>
2025-06-05 11:14:29 -0600

    Agregado tailwind para los estilos

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,22 +4,24 @@
 
 @section('content')
 
-    <div>
-        <a href="{{ route('tasks.create') }}">Agregar tarea</a>
-    </div>
+    <nav class="mb-4">
+        <a href="{{ route('tasks.create') }}"
+        class="font-medium text-gray-700 underline decoration-pink-500">Agregar tarea</a>
+    </nav>
 
     @forelse($tasks as $task)
         <div>
-             <a href="{{ route('tasks.show', ['task' => $task->id]) }}"> {{ $task-> title }} </a>
+             <a href="{{ route('tasks.show', ['task' => $task->id]) }}"
+                @class(['line-through' => $task -> completed])> {{ $task-> title }} </a>
         </div>
     @empty
         <div>No hay tareas</div>
     @endforelse
 
     @if ($tasks -> count())
-        <div>
+        <nav class="mt-4">
             {{ $tasks -> links() }}
-        </div>
+        </nav>
     @endif
 
 @endsection
